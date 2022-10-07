6e30238bde86ed51b2e6538fb9e4754685b9d96a
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2022-10-08 00:05:27 +0100

    Update settings.py

--- s3palace/settings.py
+++ s3palace/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = 'django-insecure-kdoalb8fk5urv5rxfjpc%p%hi^3)qe^()h=uw4e2f3xue+o-qi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['s3palace.com', 's3palacesite.herokuapp.com']
+ALLOWED_HOSTS = ['https://s3palace.com', 's3palacesite.herokuapp.com']
+
+CSRF_TRUSTED_ORIGINS = ['https://s3palace.com']
 
 #gmail smtp
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
